13244b90e176778c4b7a822680ac0a41a7a09e59
Sebastian B <me@sebastianb.dev>
2023-02-03 08:48:17 -0500

    :tada: router working

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <NavBar/>
+  <router-view/>
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue'
+// import RestaurantMenu from './components/ExampleMenu.vue'
+
 export default {
-  name: 'App'
+  name: 'HomePage',
+  components: {
+    NavBar
+  }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
+
 import { createApp } from 'vue'
-import App from './Home.vue'
-import './index.css'
+import App from './App.vue'
+import { router } from '@/router'
 
-
-createApp(App).mount('#app')
+const app = createApp(App).use(router)
+app.mount('#app')
