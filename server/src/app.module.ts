cf9fad877227a8f368869039b6d4c16e4b6e505d
Sebastian B <me@sebastianb.dev>
2023-02-04 11:11:47 -0500

    :art: moved dev debug server broadcast

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MenuController } from './menu/menu.controller';
 import {MenuService} from "./menu/menu.service";
 
 @Module({
   imports: [],
-  controllers: [AppController, MenuController],
-  providers: [AppService, MenuService],
+  controllers: [MenuController],
+  providers: [MenuService],
 })
 export class AppModule {}
