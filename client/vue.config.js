ea3680246efd89f231b2d3b5171793be7caecf5c
Sebastian B <me@sebastianb.dev>
2023-02-03 19:10:23 -0500

    :art: installed express and split into server + clientside folders

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs')
+const YAML = require('yaml')
+
+const generalConfig = fs.readFileSync('../config.yml', 'utf8')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  devServer: { port: YAML.parse(generalConfig)["clientPort"]},
   publicPath: '',
   configureWebpack: {
     devServer: {
