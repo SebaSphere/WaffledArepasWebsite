93c17d6410cd7f679b7041371887bd9fc701ed34
Sebastian B <me@sebastianb.dev>
2023-02-04 10:22:42 -0500

    :ambulance: fix prerender later

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,7 +10,7 @@ const prerenderToken = fs.readFileSync('../credentials/prerender-token.json', 'u
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(YAML.parse(generalConfig)["serverPort"]);
-  app.use(require('prerender-node').set('prerenderToken', JSON.parse(prerenderToken)["token"]));
+  // app.use(require('prerender-node').set('prerenderToken', JSON.parse(prerenderToken)["token"]));
   console.log("The server IP is http://127.0.0.1:" + YAML.parse(generalConfig)["serverPort"])
 }
 bootstrap();
