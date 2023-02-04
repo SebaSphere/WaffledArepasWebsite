faf306701a4149e3d6e4d17db80e770c32a0997a
Sebastian B <me@sebastianb.dev>
2023-02-04 11:11:35 -0500

    :art: moved dev debug server broadcast

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(YAML.parse(generalConfig)["serverPort"]);
   // app.use(require('prerender-node').set('prerenderToken', JSON.parse(prerenderToken)["token"]));
-  console.log("The server IP is http://127.0.0.1:" + YAML.parse(generalConfig)["serverPort"])
 }
-bootstrap();
+bootstrap().then(function () {
+  console.log("The server IP is http://127.0.0.1:" + YAML.parse(generalConfig)["serverPort"])
+});
