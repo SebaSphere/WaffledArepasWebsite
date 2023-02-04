7458dc8327959f1c57d6a7e5bbbc29dfd582ecb5
Sebastian B <me@sebastianb.dev>
2023-02-04 10:15:01 -0500

    :mag: basic prerender setup, I doubt it works

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,10 +5,12 @@ const fs = require('fs')
 const YAML = require('yaml')
 
 const generalConfig = fs.readFileSync('../config.yml', 'utf8')
+const prerenderToken = fs.readFileSync('../credentials/prerender-token.json', 'utf8')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(YAML.parse(generalConfig)["serverPort"]);
+  app.use(require('prerender-node').set('prerenderToken', JSON.parse(prerenderToken)["token"]));
   console.log("The server IP is http://127.0.0.1:" + YAML.parse(generalConfig)["serverPort"])
 }
 bootstrap();
