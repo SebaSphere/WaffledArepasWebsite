6d9ee4fe07a0d75cc29fe935aa1c156348bedfb4
Sebastian B <me@sebastianb.dev>
2023-02-12 05:17:30 -0500

    :sparkles: Docker compose on the client, server, and database
    Signed-off-by: Sebastian B <me@sebastianb.dev>

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,13 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+
 const fs = require('fs')
 const YAML = require('yaml')
-
-const generalConfig = fs.readFileSync('../config.yml', 'utf8')
 
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  devServer: { port: YAML.parse(generalConfig)["clientPort"]},
   publicPath: '',
   configureWebpack: {
     devServer: {
